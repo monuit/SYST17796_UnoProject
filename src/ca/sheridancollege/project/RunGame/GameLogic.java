445b858c9f0c7f09e2547e044d33523b244db97d
corpsema <73707049+mo-a114@users.noreply.github.com>
2021-04-10 13:44:10 -0400

    added userTurnToPlay method which checks which user it is to play for that round, checks if they win, and checks if they were impacted by any special cards

--- src/ca/sheridancollege/project/RunGame/GameLogic.java
+++ src/ca/sheridancollege/project/RunGame/GameLogic.java
@@ -35,4 +35,140 @@ public class GameLogic {
 	// Winner of the game
 	private String winnerOfGame;
 
+	// This will start up the game
+	public GameLogic()
+	{
+		firstDeck = new Deck();
+		computerOne = new Hand("Computer 1");
+		userPlayer = new Hand("The user");
+
+		userInput = new BufferedReader(new InputStreamReader(System.in));
+
+		SetUpTheGame();
+		StartGame();
+	}
+	// This will print if someone wins. It will get the game started with user playing first.
+	private void StartGame()
+	{
+		try {
+			userTurnToPlay();
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+		for (String s : Arrays.asList(winnerOfGame + " has won", "END")) {
+			System.out.println(s);
+		}
+	}
+
+	/**
+	 * Lets the user go through their turn and checks if they win.
+	 * At beginning of turn checks if player was affected by any special cards
+	 */
+	private void userTurnToPlay() throws IOException {
+		if (centerCardAffected instanceof DrawFour && !centerCardAffected.getUsed()) {
+			for (int i = 0; i < 4; i++) {
+				userPlayer.addToHand(firstDeck.drawRandomCard());
+			}
+			centerCardAffected.setUsed();
+			System.out.println("Next player drew 4 cards");
+		}
+		if (centerCardAffected instanceof DrawTwo && !centerCardAffected.getUsed()) {
+			for (int i = 0; i < 2; i++) {
+				userPlayer.addToHand(firstDeck.drawRandomCard());
+			}
+			centerCardAffected.setUsed();
+			System.out.println("Next player drew 2 cards");
+		}
+		int choiceOfCardUserPicked = 0; //The index of the card the user picks
+		try {
+			choiceOfCardUserPicked = askForUserInput();
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+
+		if (choiceOfCardUserPicked == -1) { //User wants to draw a card from the deck which is random
+			userPlayer.addToHand(firstDeck.drawRandomCard());
+			System.out.println("You drew a card!");
+			nextPlayer = checkNextPlayer();
+			computersPlay();
+
+		} else if (choiceOfCardUserPicked == -2) { //User wants to quit the game
+			winnerOfGame = "No one";
+			try {
+				userInput.close();
+			} catch (IOException e) {
+				e.printStackTrace();
+			}
+			System.out.println("You quit the game!");
+
+		} else //User wants to play a card from their index (hand)
+			if (!validUserInput(userPlayer, choiceOfCardUserPicked)) {
+				userTurnToPlay();
+			} else {
+
+				centerCardAffected = userPlayer.getCard(choiceOfCardUserPicked);
+				userPlayer.removeFromHand(choiceOfCardUserPicked);
+				System.out.println("You played: " + centerCardAffected);
+
+				if (centerCardAffected instanceof WildCard || centerCardAffected instanceof DrawFour) {
+					System.out.println("Pick a color from: Yellow, Green, Blue, Red");
+					String newColorChosen = "Yellow";
+
+					if (userInput.ready()) {
+						newColorChosen = userInput.readLine();
+					} else {
+						System.out.println("Game Ended - Idle");
+						userInput.close();
+						System.exit(0);
+					}
+
+					if (!newColorChosen.equalsIgnoreCase("Red") && !newColorChosen.equalsIgnoreCase("Green")
+							&& !newColorChosen.equalsIgnoreCase("Blue") &&
+							!newColorChosen.equalsIgnoreCase("Yellow")) {
+						do { // Makes sure color input is valid
+							System.out.println(newColorChosen + " is not a valid color!\nInput a new color:");
+							if (userInput.ready()) {
+								newColorChosen = userInput.readLine();
+							} else {
+								System.out.println("Game Ended - Idle");
+								userInput.close();
+								System.exit(0);
+							}
+						} while (!newColorChosen.equalsIgnoreCase("Red") &&
+								!newColorChosen.equalsIgnoreCase("Green")
+								&& !newColorChosen.equalsIgnoreCase("Blue") &&
+								!newColorChosen.equalsIgnoreCase("Yellow"));
+					}
+
+					System.out.println("The color is now: " + newColorChosen);
+					centerCardAffected.setColorOfCard(newColorChosen);
+				}
+
+				// Special card effects
+				if (centerCardAffected instanceof Skip && !centerCardAffected.getUsed()) {
+					nextPlayer++;
+					centerCardAffected.setUsed();
+				}
+				if (centerCardAffected instanceof Reverse && !centerCardAffected.getUsed()) {
+					clockWiseDirection = !clockWiseDirection;
+					centerCardAffected.setUsed();
+				}
+
+				hasAnyPlayerWon = checkForWinner(userPlayer);
+				if (!hasAnyPlayerWon) //Checks if the user has not won, then lets the computer play
+				{
+					nextPlayer = checkNextPlayer();
+					computersPlay();
+				} else {
+					try {
+						userInput.close();
+					} catch (IOException e) {
+						e.printStackTrace();
+					}
+					winnerOfGame = "The user";
+				}
+
+			}
+	}
+
 }
