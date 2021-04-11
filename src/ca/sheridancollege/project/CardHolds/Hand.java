32c4cde9557dbcb9d662966495a563dc5e487653
corpsema <73707049+mo-a114@users.noreply.github.com>
2021-04-11 17:54:30 -0400

    added toString method to show the hand of the player at the appropriate time

--- src/ca/sheridancollege/project/CardHolds/Hand.java
+++ src/ca/sheridancollege/project/CardHolds/Hand.java
@@ -4,7 +4,7 @@
  * @date March 28, 2021
  */
 
-package ca.sheridancollege.project.CardHolds;
+package CardHolds;
 import java.util.ArrayList;
 import ca.sheridancollege.project.Cards.Card;
 
@@ -20,7 +20,7 @@ public class Hand {
 	public Hand(String name)
 	{
 		this.name = name;
-		HandArray = new ArrayList<Card>();
+		HandArray = new ArrayList<>();
 	}
 	
 	/**Return the name of this hand's owner*/
@@ -41,19 +41,19 @@ public class Hand {
 	/**
 	 * Adds a card to the array that is your hand
 	 */
-	public void addToHand(Card q)
+	public void addToHand(Card Card)
 	{
             
-		HandArray.add(q);
+		HandArray.add(Card);
             
 	}
 	
 	/**
 	 * Removes card from the array that is your hand
 	 */
-	public void removeFromHand(int q)
+	public void removeFromHand(int Card)
 	{
-		HandArray.remove(q);
+		HandArray.remove(Card);
 	}
 	
 	public int getSize()
@@ -64,9 +64,26 @@ public class Hand {
 	/**
 	 * Prints what your hand contains at that time
 	 */
-        @Override
 	public String toString() {
-		return null;
+			{
+				StringBuilder returned = new StringBuilder("Your hand contains: \n");
+				int counter = 1;
+
+				for(Card Card: HandArray)
+				{
+					if(counter == HandArray.size())
+					{
+						returned.append("[").append(counter).append("]").append(x);
+					}
+					else
+					{
+						returned.append("[").append(counter).append("]").append(x).append("\n");
+						counter++;
+					}
+				}
+
+				return returned.toString();
+			}
 	}
 
 }
